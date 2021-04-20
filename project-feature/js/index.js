76a5bf21031f0a7d06c72a6c669dd4dd8cdad663
Gadjik21 <63861277+Gadjik21@users.noreply.github.com>
2021-04-20 20:50:21 +0300

    Add files via upload

---
+++ project-feature/js/index.js
@@ -0,0 +1,38 @@
+    $(document).ready(function(){
+  $('.features-slider').slick({
+  infinite: true,
+  speed: 300,
+  slidesToShow: 4,
+  slidesToScroll: 4,
+  responsive: [
+    {
+      breakpoint: 1040,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        prevArrow: '<button class="prev"></button>',
+        nextArrow: '<button class="next"></button>',
+      }
+    },
+    {
+      breakpoint: 767,
+      settings: {
+        slidesToShow: 1,
+        prevArrow: '<button class="prev"></button>',
+        slidesToScroll: 1,
+        nextArrow: '<button class="next"></button>',
+      }
+    },
+    {
+      breakpoint: 500,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        prevArrow: '<button class="prev"></button>',
+        nextArrow: '<button class="next"></button>',
+      }
+    }
+  ]
+});
+	
+});
